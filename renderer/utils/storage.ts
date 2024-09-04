0bbb897ed2e457c1598c0d486f226dc830069b36
cutls <p@cutls.dev>
2024-09-05 00:54:30 +0900

    no Array expression

--- renderer/utils/storage.ts
+++ renderer/utils/storage.ts
@@ -1,23 +1,23 @@
 import type { Account } from '@/entities/account'
 import type { Server } from '@/entities/server'
 import { type Settings, defaultSetting } from '@/entities/settings'
-import { type AddTimeline, type Color, type ColumnWidth, type Timeline, colorList, columnWidthSet } from '@/entities/timeline'
-import { localTypeList, type localeType } from '@/i18n'
+import { type AddTimeline, type Color, type Timeline, colorList } from '@/entities/timeline'
+import { localTypeList } from '@/i18n'
 import { detector } from '@cutls/megalodon'
 
-export async function listTimelines(): Promise<Array<[Timeline, Server]>> {
+export async function listTimelines(): Promise<[Timeline, Server][][]> {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[][] = JSON.parse(timelinesStr || '[]')
 	const serversStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serversStr || '[]')
-	return timelines.map((timeline) => [timeline, servers.find((server) => server.id === timeline.server_id)])
+	const servers: Server[] = JSON.parse(serversStr || '[]')
+	return timelines.map((oneColumn) => oneColumn.map((timeline) => [timeline, servers.find((server) => server.id === timeline.server_id)]))
 }
 
-export async function listAccounts(): Promise<Array<[Account, Server]>> {
+export async function listAccounts(): Promise<[Account, Server][]> {
 	const accountsStr = localStorage.getItem('accounts')
-	const accounts: Array<Account> = JSON.parse(accountsStr || '[]')
+	const accounts: Account[] = JSON.parse(accountsStr || '[]')
 	const serversStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serversStr || '[]')
+	const servers: Server[] = JSON.parse(serversStr || '[]')
 	const accountFiltered = accounts.filter((account) => servers.findIndex((server) => server.account_id === account.id) >= 0)
 	localStorage.setItem('accounts', JSON.stringify(accountFiltered))
 	return accountFiltered.map((account) => [account, servers.find((server) => server.account_id === account.id)])
@@ -25,7 +25,7 @@ export async function listAccounts(): Promise<Array<[Account, Server]>> {
 
 export async function addTimeline(server: Server, timeline: AddTimeline): Promise<void> {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	timelines.push({
 		id: timelines.length + 1,
 		kind: timeline.kind,
@@ -41,24 +41,24 @@ export async function addTimeline(server: Server, timeline: AddTimeline): Promis
 
 export async function removeTimeline({ id }: { id: number }): Promise<void> {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const newTimelines = timelines.filter((timeline) => timeline.id !== id)
 	const newOrderTimelines = newTimelines.map((timeline, index) => ({ ...timeline, id: index + 1 }))
 	localStorage.setItem('timelines', JSON.stringify(newOrderTimelines))
 	return
 }
 
-export async function listServers(): Promise<Array<[Server, Account]>> {
+export async function listServers(): Promise<[Server, Account][]> {
 	const serversStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serversStr || '[]')
+	const servers: Server[] = JSON.parse(serversStr || '[]')
 	const accountsStr = localStorage.getItem('accounts')
-	const accounts: Array<Account> = JSON.parse(accountsStr || '[]')
+	const accounts: Account[] = JSON.parse(accountsStr || '[]')
 	return servers.map((server) => [server, accounts.find((account) => server.account_id === account.id)])
 }
 
 export async function addServer({ domain }: { domain: string }): Promise<Server> {
 	const serversStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serversStr || '[]')
+	const servers: Server[] = JSON.parse(serversStr || '[]')
 	const serverMaxId = servers.reduce((max, server) => (server.id > max ? server.id : max), 0)
 	const sns = await detector(`https://${domain}`)
 	if (sns === 'gotosocial') return
@@ -76,25 +76,25 @@ export async function addServer({ domain }: { domain: string }): Promise<Server>
 }
 export async function getServer({ id }: { id: number }): Promise<Server> {
 	const serversStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serversStr || '[]')
+	const servers: Server[] = JSON.parse(serversStr || '[]')
 	const server = servers.find((server) => server.id === id)
 	return server
 }
 export async function removeServer({ id }: { id: number }): Promise<void> {
 	const serverStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serverStr || '[]')
+	const servers: Server[] = JSON.parse(serverStr || '[]')
 	const targetServer = servers.find((server) => server.id === id)
 	const deleteAccount = targetServer ? targetServer.account_id : null
 	if (deleteAccount) {
 		const accountsStr = localStorage.getItem('accounts')
-		const accounts: Array<Account> = JSON.parse(accountsStr || '[]')
+		const accounts: Account[] = JSON.parse(accountsStr || '[]')
 		const newAccounts = accounts.filter((account) => account.id !== deleteAccount)
 		localStorage.setItem('accounts', JSON.stringify(newAccounts))
 	}
 	const newServers = servers.filter((server) => server.id !== id)
 	localStorage.setItem('servers', JSON.stringify(newServers))
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const newTimeline = timelines.filter((timeline) => timeline.server_id !== id)
 	localStorage.setItem('timelines', JSON.stringify(newTimeline))
 	return
@@ -130,10 +130,10 @@ export async function saveSetting({ obj }: { obj: Settings }): Promise<void> {
 }
 export async function getAccount({ id }: { id: number }): Promise<[Account, Server]> {
 	const serversStr = localStorage.getItem('servers')
-	const servers: Array<Server> = JSON.parse(serversStr || '[]')
+	const servers: Server[] = JSON.parse(serversStr || '[]')
 	const server = servers.find((server) => server.account_id === id)
 	const accountsStr = localStorage.getItem('accounts')
-	const accounts: Array<Account> = JSON.parse(accountsStr || '[]')
+	const accounts: Account[] = JSON.parse(accountsStr || '[]')
 	const account = accounts.find((account) => account.id === id)
 	return [account, server]
 }
@@ -147,7 +147,7 @@ export async function getUsualAccount(): Promise<number> {
 }
 export async function updateColumnWidth({ id, columnWidth }: { id: number; columnWidth: number }) {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const timeline = timelines.find((timeline) => timeline.id === id)
 	timeline.column_width = columnWidth
 	localStorage.setItem('timelines', JSON.stringify(timelines))
@@ -156,7 +156,7 @@ export async function updateColumnWidth({ id, columnWidth }: { id: number; colum
 const isColorGuard = (value: string): value is Color => colorList.includes(value as any)
 export async function updateColumnColor({ id, color }: { id: number; color: string }) {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const timeline = timelines.find((timeline) => timeline.id === id)
 	if (!timeline) return
 	if (!isColorGuard(color)) {
@@ -169,7 +169,7 @@ export async function updateColumnColor({ id, color }: { id: number; color: stri
 }
 export async function updateColumnOrder({ id, direction }: { id: number; direction: 'left' | 'right' }) {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const timelineIndex = timelines.findIndex((timeline) => timeline.id === id)
 	const nextIndex = direction === 'left' ? timelineIndex - 1 : timelineIndex + 1
 	if (nextIndex < 0 || nextIndex >= timelines.length) return
@@ -182,7 +182,7 @@ export async function updateColumnOrder({ id, direction }: { id: number; directi
 }
 export async function updateAccountColor({ id, color }: { id: number; color: string }) {
 	const accountsStr = localStorage.getItem('accounts')
-	const accounts: Array<Account> = JSON.parse(accountsStr || '[]')
+	const accounts: Account[] = JSON.parse(accountsStr || '[]')
 	const account = accounts.find((account) => account.id === id)
 	if (!account) return
 	if (!isColorGuard(color)) {
@@ -196,7 +196,7 @@ export async function updateAccountColor({ id, color }: { id: number; color: str
 }
 export async function updateColumnTts({ id, toggle }: { id: number; toggle: boolean }) {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const timeline = timelines.find((timeline) => timeline.id === id)
 	if (!timeline) return
 	timeline.tts = toggle
@@ -205,7 +205,7 @@ export async function updateColumnTts({ id, toggle }: { id: number; toggle: bool
 }
 export async function updateColumnMediaOnly({ id, toggle }: { id: number; toggle: boolean }) {
 	const timelinesStr = localStorage.getItem('timelines')
-	const timelines: Array<Timeline> = JSON.parse(timelinesStr || '[]')
+	const timelines: Timeline[] = JSON.parse(timelinesStr || '[]')
 	const timeline = timelines.find((timeline) => timeline.id === id)
 	if (!timeline) return
 	timeline.mediaOnly = toggle
